da627af95656fdce65f44d97da12661366636cf9
khadjafatima409 <khadijafatima530@gmail.com>
2026-02-25 15:37:12 +0500

    Add Student model and initial migration for student data management

--- students/models.py
+++ students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    age = models.IntegerField()
+    course = models.CharField(max_length=100)
+    address = models.TextField()
+    date_joined = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
